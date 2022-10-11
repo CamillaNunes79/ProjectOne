70ee6274a95fe12ea1d27e4ed7bb32366faaddc3
Camilla Nunes <camillanunes79@gmail.com>
2022-10-11 19:19:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+  state = {
+    name: 'Camilla Nunes',
+    counter: 0
+  };
+
+
+handlePClick = () => {
+  this.setState({ name: 'Nascimento' });
+}
+
+handleAClick = (event) => {
+  event.preventDefault();
+  const { counter } = this.state;
+  this.setState({ counter: counter + 1 });
+}
+
+render() {
+
+  const { name, counter } = this.state
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.handlePClick}>
+          {name} {counter}
         </p>
         <a
+          onClick={this.handleAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          linkzin
         </a>
       </header>
     </div>
   );
 }
+}
 
-export default App;
+export default App
+;
